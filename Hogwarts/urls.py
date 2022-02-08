c8351cb429bd6099a53cec3ccb667fa7c53d8ff2
Hieu Truong <hieu.tnxd.bk@gmail.com>
2022-02-08 11:04:08 +0700

    commit

--- Hogwarts/urls.py
+++ Hogwarts/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include, re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Wizard.urls')),
-    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
